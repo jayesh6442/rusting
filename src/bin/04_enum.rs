d115a83fcd3b77d8196ce703f739fe8371c4d89c
jayesh <jayeshsarvaiya9913@gmail.com>
2026-03-30 18:34:38 +0530

    added enums

--- src/bin/04_enum.rs
+++ src/bin/04_enum.rs
@@ -1,4 +1,35 @@
+enum Status {
+    Pending,
+    Online,
+    Offline,
+    Complited,
+    Processing,
+    Finished,
+}
+
+enum Data {
+    A(i32),
+    B(i64),
+}
+
+enum ApiResonse {
+    Success { data: String },
+    Error { code: u32, message: String },
+}
 fn main() {
     println!("hi there this is enum file");
-    
+    #[allow(unused_variables)]
+    let api_response: ApiResonse = ApiResonse::Success {
+        data: String::from("here is your data"),
+    };
+    let api_response: ApiResonse = ApiResonse::Error {
+        code: 404,
+        message: String::from("not found htis "),
+    };
+
+    match api_response {
+        ApiResonse::Success { data } => println!("{}", data),
+        ApiResonse::Error { code, message } => println!("{} {}", code, message),
+        _ => print!("nothing matched for you"),
+    }
 }
