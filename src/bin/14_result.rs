c6424dccaf8af695a2772511ffcdfe99c28a0f39
jayesh <jayeshsarvaiya9913@gmail.com>
2026-03-31 17:51:42 +0530

    proj

---
+++ src/bin/14_result.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("testing");
+    let ok: Result<i32, &str> = Result::Ok(4);
+
+    let er: Result<i32, &str> = Result::Err("some error");
+    println!("{:?}", ok.unwrap());
+    println!("{:?}", er.unwrap());
+}
